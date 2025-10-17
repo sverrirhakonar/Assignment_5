824778576e6cfeeaf82b6d000a9c0b6026a8e479
sverrirhakonar <sverrirhak@gmail.com>
2025-10-16 22:13:07 -0500

    bru

---
+++ tests/test_smoke.py
@@ -0,0 +1,9 @@
+def test_math_addition():
+    # This is the *test function name*. Pytest will auto-discover it
+    # because it starts with "test_".
+    #
+    # The body of the test uses a plain Python "assert".
+    # If the condition is True, the test passes.
+    # If the condition is False, the test fails.
+
+    assert 1 + 1 == 2
